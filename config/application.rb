a29c48164eb07d65a9fbbbc4e69e66bb3d81c796
KitamuraKen <k.ken0324.soccer2610@gmail.com>
2023-06-02 08:33:02 +0900

    rails g を行った際に不要なファイルを

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,15 @@ module WonderfulPostApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.generators do |g|
+      g.jbuilder false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
+      g.test_framework false
+    end
+
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
