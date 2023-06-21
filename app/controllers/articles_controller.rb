a561f21fc68a661da674f76ed2a067c5e80983e8
KitamuraKen <k.ken0324.soccer2610@gmail.com>
2023-06-21 23:28:26 +0900

    マイページを作成した。

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -61,7 +61,8 @@ class ArticlesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
-      @article = current_user.articles.find(params[:id])
+      # @article = current_user.articles.find(params[:id])
+      @article = Article.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
