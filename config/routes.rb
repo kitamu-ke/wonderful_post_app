d2d0f90e052e0c7591ffb5bceaaa79678c335ba1
KitamuraKen <k.ken0324.soccer2610@gmail.com>
2023-06-21 23:31:32 +0900

    sample_articlesを削除した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :articles
-  resources :sample_articles
   root :to => 'articles#index'
   get 'mypage', to: "mypage#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
