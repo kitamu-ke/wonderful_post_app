83c4c6ab52345a16009ba4087710c4b325d67e1d
KitamuraKen <k.ken0324.soccer2610@gmail.com>
2023-06-25 09:02:01 +0900

    検索機能を追加した

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,8 +1,9 @@
 class MypageController < ApplicationController
 
   def index
-    @articles = current_user.articles
-    @articles = Article.page(params[:page]).per(10)
+    articles = current_user.articles
+    articles = articles.where("title LIKE ?", "%#{params[:title]}%") if params[:title].present?
+    @articles = articles.page params[:page]
   end
 
 end
